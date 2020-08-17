3c11f967a53ee805601a695a712f31182004a1b4
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-08-17 01:01:59 +0100

    add photo syncing

--- install.sh
+++ install.sh
@@ -1,36 +1,45 @@
-cleanTasks() {
-    for task in ~/.shortcuts/tasks/*
+#!/bin/bash
+cleanDir() {
+    for file in "$1"/*
     do
-        if [ -h "$task" ] && ! [ -e "$task" ]; then
-            echo -e "\033[31mRemoving broken task: $(basename "$task")\033[0m"
-            rm "$task"
+        if [ -h "$file" ] && ! [ -e "$file" ]
+        then
+            echo  "\033[31mRemoving broken file: $(basename "$file")\033[0m"
+            rm "$file"
         fi
     done
     return 1;
 }
 
-newTasks() {
-    for task in tasks/*; do
-        [ -h ~/.shortcuts/tasks/"$(basename "$task")" ] || return 0
+newDir() {
+    for file in "$1"/*
+    do
+        [ -h "$2"/"$(basename "$file")" ] || return 1
     done
-    return 1;
+    return 0;
+}
+
+syncDir() {
+    echo -e "\033[35mStoring $1...\033[0m"
+
+    for file in "$1"/*
+    do
+        file_name=$(basename "$file")
+        if ! [ -h "$2"/"$file_name" ]; then
+            echo -e "\033[33m\t$file_name\033[0m"
+            ln -s "$(pwd)/$file" "$2"/"$file_name"
+        fi
+    done
+    chmod +x "$1"/*
 }
 
 mkdir -p ~/.shortcuts/tasks
 cd "$(dirname "$(realpath "$0")")" || return 0
 
-cleanTasks
-newTasks || exit 0
+cleanDir ~/.shortcuts/tasks
+cleanDir ~/.shortcuts
 
-echo -e "\033[35mStoring Tasks...\033[0m"
+newDir tasks ~/.shortcuts/tasks || syncDir tasks ~/.shortcuts/tasks
+newDir foreground_tasks ~/.shortcuts || syncDir foreground_tasks ~/.shortcuts
 
-for task in tasks/*
-do
-    task_name=$(basename "$task")
-    if ! [ -h ~/.shortcuts/tasks/"$task_name" ]; then
-        echo -e "\033[33m\t$task_name\033[0m"
-        ln -s "$(pwd)/$task" ~/.shortcuts/tasks/"$task_name"
-    fi
-done
-chmod +x tasks/*
 echo -e "\033[35mDone!\033[0m"
