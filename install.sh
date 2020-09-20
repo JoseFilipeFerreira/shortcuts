0107c41a95cc481107e9792b0dc5e679a50c263d
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-09-21 00:50:16 +0100

    simplify instalation

--- install.sh
+++ install.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 DIR=$(dirname "$(readlink -f "$0")")
 
-cleanDir() {
-    for file in "$1"/*
-    do
-        if [ -h "$file" ] && ! [ -e "$file" ]
-        then
-            echo  "\033[31mRemoving broken file: $(basename "$file")\033[0m"
-            rm "$file"
-        fi
-    done
-    return 1;
-}
-
-newDir() {
-    for file in "$1"/*
-    do
-        [ -h "$2"/"$(basename "$file")" ] || return 1
-    done
-    return 0;
-}
-
-syncDir() {
-    echo -e "\033[35mStoring $1...\033[0m"
-
-    for file in "$1"/*
-    do
-        file_name=$(basename "$file")
-        if ! [ -h "$2"/"$file_name" ]; then
-            echo -e "\033[33m\t$file_name\033[0m"
-            ln -s "$(pwd)/$file" "$2"/"$file_name"
-        fi
-    done
-    chmod +x "$1"/*
-}
-
 task_set() {
     echo -e "\033[35mSetting task:\033[33m $1 (every: $2 ms)\033[0m"
     termux-job-scheduler \
@@ -43,17 +9,9 @@ task_set() {
         --persisted true
 }
 
-
-mkdir -p ~/.shortcuts/tasks
-cd "$(dirname "$(realpath "$0")")" || return 0
-
-cleanDir ~/.shortcuts/tasks
-cleanDir ~/.shortcuts
-
-newDir tasks ~/.shortcuts/tasks || syncDir tasks ~/.shortcuts/tasks
-newDir foreground_tasks ~/.shortcuts || syncDir foreground_tasks ~/.shortcuts
-
 termux-job-scheduler --cancel-all
-task_set foreground_tasks/change_lock 900000
+task_set tasks/change_lock 900000
 
+echo -e "\033[35mStoring Tasks...\033[33m"
+    ln -fvsn "$DIR/tasks" ~/.shortcuts | sed "s|$HOME|~|g;s|'||g"
 echo -e "\033[35mDone!\033[0m"
